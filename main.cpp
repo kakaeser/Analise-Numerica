abe80f4cb0bef73c5189d3c198a03af1242836c0
kakaeser <eduardokaeser11@gmail.com>
2025-03-19 19:17:49 -0300

    Create main.cpp
    Eu já tinha começado a fazer :)

---
+++ main.cpp
@@ -0,0 +1,190 @@
+#include <iostream>
+#include <cmath>
+#include <fstream>
+#include <iomanip>
+
+
+
+void clearScreen() {
+   #ifdef _WIN32
+       system("cls"); // Para Windows
+   #else
+       std::cout << "\033[2J\033[H"; // Para Linux/MacOS
+   #endif
+   }
+
+void Gauss(int Nsl){
+     double gab[Nsl][Nsl+1], gx[Nsl], gabe[Nsl][Nsl+1]; // Matriz A + vector B e o vetor solução, vetor para subsitruição de linhas, matriz pra calcular o erro respectivamente
+
+      //Coleta de informações da matriz A
+      for(int i = 0; i <= Nsl - 1; i++){
+         std:: cout << "Escreva a " << i+1 << "ª linha da matriz A (Ax = B):\t";
+         for(int j = 0; j <= Nsl - 1 ; j++){
+            std::cin >> gab[i][j];
+            gabe[i][j] = gab[i][j];
+         }   
+               
+         }
+      clearScreen();
+      //Coleta de informações da matriz B
+      std:: cout << "Escreva a matriz B (Ax = B):\t";
+      for(int i = 0; i <= Nsl - 1 ; i++){
+         std::cin >> gab[i][Nsl];
+         gabe[i][Nsl] = gab[i][Nsl];
+      }
+      clearScreen();
+      
+          // Matriz printer
+          for (int i = 0; i <= Nsl - 1; i++) {
+             std::cout << std::fixed << std::setprecision(4);
+             std::cout << "| ";
+             for (int j = 0; j <= Nsl - 1; j++) {
+               std::cout << std::setw(8) << gab[i][j] << " "; // Espaçamento uniforme
+            }
+            std::cout << "| \t|x" << i << "| \t|" << std::setw(8) << gab[i][Nsl] << " |" << std::endl;
+         }
+
+      
+         for (int i = 0; i < Nsl; i++) {
+              int ig = i; //Indice da linha que contem o maior valor da primeira coluna
+              double ag = std::abs(gab[i][i]); // Maior Valor da Coluna atual
+
+              // Encontrar o maior pivô na coluna atual
+              for (int k = i + 1; k < Nsl; k++) {
+                  if (std::abs(gab[k][i]) > ag) {
+                      ag = std::abs(gab[k][i]);
+                      ig = k;
+                  }
+              }
+            // Trocar linhas se necessário
+              if (ig != i) {
+                  for (int j = 0; j <= Nsl; j++) {
+                      std::swap(gab[i][j], gab[ig][j]);
+                  }
+              }
+         }
+         // Eliminação
+         for(int i = 0; i< Nsl;i++){
+            double factor;   
+            for(int k = i+1; k < Nsl;k++){
+               factor = gab[k][i]/gab[i][i];
+               for(int j = 0; j < Nsl+1; j++)  
+                  gab[k][j] -= gab[i][j]*factor;
+               
+            }
+         }
+     
+      //Matriz Printer
+      for (int i = 0; i <= Nsl - 1; i++) {
+          
+          std::cout << std::endl << "\n| ";
+          for (int j = 0; j <= Nsl - 1; j++) {
+              std::cout << std::setw(8) << gab[i][j] << " "; // Espaçamento uniforme
+          }
+          std::cout << "| \t|x" << i << "| \t|" << std::setw(8) << gab[i][Nsl] << " |" << std::endl;
+      }
+
+      
+      double detg = 1; //Determinante da matriz, para conferir se a matriz tem um, infinitas ou nenhuma solução real
+      for(int i = 0; i <= Nsl -1; i++)
+         detg = detg * gab[i][i];
+      if(detg == 0) // Caso o determinante seja 0, ou o sistema tem infinitas soluções, ou o sistema não tem solução real
+      std::cout <<"\n\nEsse sistema não tem solução única ou não tem solução real";
+         
+         //Caso a matriz tenha solução única(det!=0), o programa continua o cálculo
+      else {
+         //Substituição retroativa
+         double xacumulado; // Soma parcial das colunas
+         
+         //Zerador do vetor solução pra
+         for(int i=0; i < Nsl;i++)
+            gx[i]=0;
+         
+         for(int i = Nsl-1; i>=0 ;i--){
+            xacumulado = 0;
+            for(int j = 0; j < Nsl; j++)
+               if(j != i)
+                  xacumulado +=  gx[j]*gab[i][j];
+               
+            gx[i] = (gab[i][Nsl]-xacumulado)/gab[i][i];
+         }
+         //Printer da solução
+         std::cout << std::fixed << std::setprecision(6);
+         std::cout<< std::endl <<"\nVetor solução X = [ ";
+         for(int i = 0; i <Nsl;i++)
+            std::cout << gx[i] << ' ';
+         std::cout<< "]^t"<< std::endl;
+
+         //Calculo do erro 
+         double eg[Nsl];
+         for(int i = 0; i< Nsl;i++){
+            xacumulado = 0;
+            for(int j = 0; j <= Nsl-1; j++)
+               xacumulado +=  gx[j]*gabe[i][j];
+            eg[i]= gabe[i][Nsl] - xacumulado;
+         }
+         //Printer do erro
+         std::cout<< std::endl <<"r (erro residual) = [ ";
+         for(int i = 0; i <Nsl;i++)
+            std::cout << eg[i] << ' ';
+         std::cout<< "]^t"<< std::endl;
+      }
+   }
+
+
+
+int main(){
+   int a = 0;
+   std::cout << "O que você quer calcular?" << std:: endl << "1 - Sistemas Lineares" << std:: endl << "2 - Interpolação Polinomial" << std:: endl;
+   std::cout << "3 - Ajustes de curvas" << std:: endl << "4 - Integração numérica" << std:: endl << "5 - Raizes de polinômio" << std::endl;
+   do {
+      std::cout << std::endl << "Escreva o numero de 1 até 5" << std::endl;
+      std::cin >> a;
+   } while(a < 1 || a > 5);
+  
+   clearScreen();
+   if(a == 1){ // Sistemas Lineares com matrizes
+
+      int sl = 0; // Selecionador de método 
+      int Nsl; // Quantidade de x's
+      
+      std::cout << "Quantas variaveis tem o seu sistema?" << std::endl;
+      std::cin >> Nsl; // Definir o tamanho da matriz, vetor solução e etc
+
+      clearScreen();
+      
+      std::cout << "Qual método será utilizado?\n" << "1 - Eliminação de Gauss\n" << "2 - Decomposição LU\n" << "3 - Choelesky (Recomendado apenas para matrizes positivas e simétricas)\n";
+
+      do {
+      std::cout << std::endl << "Escreva o numero de 1 até 3" << std::endl;
+      std::cin >> sl;
+         
+      } while(sl < 1 || sl > 3); //Definir qual método será utilizado 
+
+      clearScreen();
+      
+      if(sl == 1){ // Gauss
+         Gauss(Nsl);
+      
+      } else if(sl == 2){ // LU
+         
+      } else if(sl == 3){ // Choelesky
+         
+      }
+
+      
+   } else if (a == 2){ // Interpolação polinomial
+      std::cout << "Não temos ainda";
+   } else if (a == 3){ // Ajuste de Curva
+      std::cout << "Não temos ainda";
+   } else if (a == 4){ // Integração numérica
+      std::cout << "Não temos ainda";
+   } else if (a == 5){ // Raízes de polinômio
+      std::cout << "Não temos ainda";
+   }
+
+
+
+
+   
+}
